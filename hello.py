d0d6f800028b1647577abd74cbfbf8f6fe395e07
Chip Childers <chip.childers@gmail.com>
2015-08-12 14:49:20 -0400

    Cleaning up file

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-"""Cloud Foundry test"""
 from flask import Flask
 import os
 
@@ -8,12 +7,7 @@ cfindex = os.getenv("INSTANCE_INDEX")
 
 @app.route('/')
 def hello_world():
-    responsestr = 'Hello World! I am instance number ' + str(cfindex)
-
-#    for key in os.environ.keys():
-#        responsestr = responsestr + "\n%30s %s \n" % (key,os.environ[key])
-
-    return responsestr
+    return = 'Hello World! I am instance number ' + str(cfindex)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)

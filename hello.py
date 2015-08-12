034a2ac16801e3bff8f4c9cd5b4e03f1c4c69bb2
Chip Childers <chip.childers@gmail.com>
2015-08-12 15:02:31 -0400

    Fix typo

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ cfindex = os.getenv("INSTANCE_INDEX")
 
 @app.route('/')
 def hello_world():
-    return = 'Hello World! I am instance number ' + str(cfindex)
+    return 'Hello World! I am instance number ' + str(cfindex)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)

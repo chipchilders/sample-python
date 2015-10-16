2d8906b31ed17c614d10f094767d7cecc22f0ece
Chip Childers <chip.childers@gmail.com>
2015-10-16 11:56:24 -0400

    another fix

--- hello.py
+++ hello.py
@@ -3,13 +3,14 @@ import os
 
 app = Flask(__name__)
 
-cfindex = os.getenv("CF_INSTANCE_INDEX")
+cfindex = os.getenv("CF_INSTANCE_INDEX", 0)
 
 svrport = os.getenv('VCAP_APP_PORT', '5000')
 
 @app.route('/')
 def hello_world():
     return 'Hello World! I am instance number ' + str(cfindex)
+#os.getenv('VCAP_SERVICES', '')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=svrport)
